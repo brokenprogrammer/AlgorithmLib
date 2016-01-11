33c4b670942b10330b86b046fd6bb9c586e98446
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-01-11 19:46:16 +0100

    Fixed StirlingsApproximitation and added LogarithmicFactorial which calculates ln(factorial)

--- src/alg.js
+++ src/alg.js
@@ -41,6 +41,13 @@ var algJS;
     var Mathematics = (function () {
         function Mathematics () {
         }
+        Mathematics.LogarithmicFactorial = function (n, val) {
+            if(n == 0) {
+                return val;
+            }
+
+            return Mathematics.LogarithmicFactorial(n-1, val + Math.log(n));
+        }
         Mathematics.EuclideansAlgorithm = function (p, q) {
             if (q == 0) {
                 return p;
@@ -48,15 +55,8 @@ var algJS;
             var remainder = p % q;
             return Mathematics.EuclideansAlgorithm(q, remainder);
         }
-        Mathematics.StirlingsApproximation = function (n, val) {
-            if(n == 0) {
-                return val;
-            }
-
-            return Mathematics.StirlingsApproximation(n-1, val + Math.log(n));
-        }
-        Mathematics.OtherStirling = function (n) {
-            return Math.sqrt((Math.PI * 2) * n) * Math.exp((n / Math.E), n);
+        Mathematics.StirlingsApproximation = function (n) {
+            return Math.sqrt((2 * Math.PI) * n) * Math.pow((n / Math.E), n);
         }
         return Mathematics;
     }());
@@ -117,6 +117,3 @@ var algJS;
     }());
     algJS.Sort = Sort;
 })(algJS || (algJS = {}));
-
-console.log(algJS.Mathematics.StirlingsApproximation(10, 0));
-console.log(algJS.Mathematics.OtherStirling(10));

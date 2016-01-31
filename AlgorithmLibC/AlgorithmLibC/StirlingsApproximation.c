2fdd7930bd392ee68da206d2c77f95313349df17
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-02-01 00:43:41 +0100

    Implemented Stirlings Approximation in C

--- AlgorithmLibC/AlgorithmLibC/StirlingsApproximation.c
+++ AlgorithmLibC/AlgorithmLibC/StirlingsApproximation.c
@@ -26,36 +26,20 @@
  * THE SOFTWARE.
  */
 
-#include <stdio.h>
-#include "BubbleSort.h"
-#include "EuclideansAlgorithm.h"
 #include "StirlingsApproximation.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    int sum;
-    
-    sum = 50 + 25;
-    
-    printf("Programming is fun!\n");
-    printf("And programming in C is even more fun!\n");
-    
-    printf("The sum of 50 and 25 is: %i\n", sum);
-    
-    int values[] = {6, 4, 1, 2, 3, 5, 7, 10, 99, 64, 32, 22, 103, 74, 8, 9};
-    int valuesSize = sizeof (values) / sizeof (int);
-    printf("Values Length: %lu \n", sizeof (values) / sizeof (int));
-    
-    BubbleSort(values, valuesSize);
-    BubbleSortHiLo(values, valuesSize);
-    
-    for (int x = 0; x < valuesSize; x++) {
-        printf("%i, ", values[x]);
-    }
-    
-    printf("Euclieands of: 252 & 105: %i\n", EuclideansAlgorithm(252, 105));
-    
-    printf("Stirlings Approximitation of !1: %f\n", StirlingsApproximation(1));
-    printf("Stirlings Approximitation of !1: %f\n", StirlingsApproximation(5));
-    return 0;
+/**
+ * Stirlings Approximitation is used as a solution to approximitate large factorial
+ * numbers. Even with programming code a big factorial number can take time to calculate,
+ * using Stirlings Approximitation formula we can approximate the factorial value.
+ * n! is that same as stirlings returned n! just that stirlings is an approximate.
+ * 
+ * n! ∼ √2πn(n / e)^n
+ *
+ * @param n - The number we should approximate the factorial for.
+ *
+ * @return Approximate factorial value of n.
+ */
+double StirlingsApproximation(double n) {
+    return sqrt((2 * M_PI) * n) * pow(n / M_E, n);
 }
--- AlgorithmLibC/AlgorithmLibC/StirlingsApproximation.h
+++ AlgorithmLibC/AlgorithmLibC/StirlingsApproximation.h
@@ -26,36 +26,12 @@
  * THE SOFTWARE.
  */
 
-#include <stdio.h>
-#include "BubbleSort.h"
-#include "EuclideansAlgorithm.h"
-#include "StirlingsApproximation.h"
+#ifndef StirlingsApproximation_h
+#define StirlingsApproximation_h
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    int sum;
-    
-    sum = 50 + 25;
-    
-    printf("Programming is fun!\n");
-    printf("And programming in C is even more fun!\n");
-    
-    printf("The sum of 50 and 25 is: %i\n", sum);
-    
-    int values[] = {6, 4, 1, 2, 3, 5, 7, 10, 99, 64, 32, 22, 103, 74, 8, 9};
-    int valuesSize = sizeof (values) / sizeof (int);
-    printf("Values Length: %lu \n", sizeof (values) / sizeof (int));
-    
-    BubbleSort(values, valuesSize);
-    BubbleSortHiLo(values, valuesSize);
-    
-    for (int x = 0; x < valuesSize; x++) {
-        printf("%i, ", values[x]);
-    }
-    
-    printf("Euclieands of: 252 & 105: %i\n", EuclideansAlgorithm(252, 105));
-    
-    printf("Stirlings Approximitation of !1: %f\n", StirlingsApproximation(1));
-    printf("Stirlings Approximitation of !1: %f\n", StirlingsApproximation(5));
-    return 0;
-}
+#include <stdio.h>
+#include <math.h>
+
+double StirlingsApproximation(double n);
+
+#endif /* StirlingsApproximation_h */

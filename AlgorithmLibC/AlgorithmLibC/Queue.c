71156ba4d042c5974fb2eb84f8a5070cb5fe375e
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-02-05 11:57:47 +0100

    Started work with Queue data structure implementation.

---
+++ AlgorithmLibC/AlgorithmLibC/Queue.c
@@ -0,0 +1,29 @@
+//
+//  Queue.c
+//  AlgorithmLibC
+//
+//  Created by Oskar Mendel on 2/4/16.
+//  Copyright © 2016 Oskar Mendel. All rights reserved.
+//
+
+#include "Queue.h"
+#include "stdlib.h"
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+typedef struct Queue {
+    struct Node *first;
+    struct Node *last;
+    int size;
+}Queue;
+
+Queue* initWithData() {
+    Queue *newQueue = (Queue*) malloc(sizeof(Queue));
+    
+    
+    
+    return newQueue;
+}
---
+++ AlgorithmLibC/AlgorithmLibC/Queue.h
@@ -0,0 +1,20 @@
+//
+//  Queue.h
+//  AlgorithmLibC
+//
+//  Created by Oskar Mendel on 2/4/16.
+//  Copyright © 2016 Oskar Mendel. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+#include <stdio.h>
+
+typedef struct Queue Queue;
+
+struct ArrayQueue;
+
+
+
+#endif /* Queue_h */

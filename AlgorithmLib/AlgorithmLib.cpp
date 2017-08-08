df7746af13013690d5b126dac8eecf12c6e95633
Oskar Mendel <brokenprogrammer@gmail.com>
2017-08-08 10:20:20 +0200

    Added stack implementation.

--- AlgorithmLib/AlgorithmLib.cpp
+++ AlgorithmLib/AlgorithmLib.cpp
@@ -1,10 +1,50 @@
 #include "simplemath.h"
 #include <iostream>
+#include "Stack.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!";
+	cout << "Hello World!" << endl;
+
+	Stack<int> myStack;
+	
+	//int myInt = myStack.getSize();
+	//cout << "My stack size: " << myInt << endl;
+	myStack.push(12);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+	myStack.push(13);
+
+	if (myStack.isEmpty()) 
+	{
+		cout << "Empty stack!" << endl;
+		cout << "Stack contains: " << myStack.getSize() << " elements." << endl;
+	}
+
+	cout << "Top element: " << myStack.peek() << endl;
+
+	cout << myStack.pop() << endl;
+	cout << myStack.pop() << endl;
+	cout << "Stack contains: " << myStack.getSize() << " elements." << endl;
+
+	myStack.clear();
+	cout << "Stack contains: " << myStack.getSize() << " elements." << endl;
+
+	int tmp = 0;
+	cin >> tmp;
 	return 0;
 }
---
+++ AlgorithmLib/Stack.h
@@ -0,0 +1,125 @@
+#ifndef Stack_H
+#define Stack_H
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+class Stack
+{
+public:
+	Stack();
+	Stack(const Stack&) = default;
+	~Stack();
+
+	Stack& operator =(const Stack &) = default;
+
+	void	 push(T const& data);
+	void     push(T&& data);
+
+	T        pop();
+
+	bool     isEmpty() const;
+	size_t   getSize() const;
+	T&       peek()    const;
+	void	 clear();
+
+	struct Node {
+		T data;
+		Node *next;
+	};
+
+private:
+	Node*  root;
+	size_t elements;
+};
+
+template<typename T>
+Stack<T>::Stack()
+{
+	root = nullptr;
+	elements = 0;
+}
+
+template<typename T>
+Stack<T>::~Stack()
+{
+	clear();
+}
+
+template<typename T>
+void Stack<T>::push(T const & data)
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->next = root;
+	root = newNode;
+	++elements;
+}
+
+template<typename T>
+void Stack<T>::push(T && data)
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->next = root;
+	root = newNode;
+	++elements;
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+	if (isEmpty())
+		throw std::runtime_error{ "Stack<T>::pop(): Empty stack" };
+
+	T value = root->data;
+
+	Node* temp = root->next;
+	delete root;
+	root = temp;
+	--elements;
+
+	return value;
+}
+
+template<typename T>
+bool Stack<T>::isEmpty() const
+{
+	return elements == 0;
+}
+
+template<typename T>
+size_t Stack<T>::getSize() const
+{
+	return elements;
+}
+
+template<typename T>
+T & Stack<T>::peek() const
+{
+	if (isEmpty())
+		throw std::runtime_error{ "Stack<T>::pop(): Empty stack" };
+
+	return root->data;
+}
+
+template<typename T>
+void Stack<T>::clear()
+{
+	if (!isEmpty()) 
+	{
+		Node* nextNode = nullptr;
+		while (root != nullptr)
+		{
+			nextNode = root->next;
+			delete root;
+			root = nextNode;
+			--elements;
+		}
+
+		root = nullptr;
+	}
+}
+
+#endif

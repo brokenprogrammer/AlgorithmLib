590cd2e95283eb75247b96f6facd1975a2630cd0
Oskar Mendel <brokenprogrammer@gmail.com>
2017-08-10 19:40:50 +0200

    Added Bubblesort implementation.

---
+++ AlgorithmLib/BubbleSort.h
@@ -0,0 +1,29 @@
+#ifndef BubbleSort_H
+#define BubbleSort_H
+#pragma once
+
+#include <vector>
+
+template <typename T>
+void sort(std::vector<T> &data)
+{
+	int len = data.size();
+	bool swapped = true;
+
+	for (int i = 0; i < len; ++i)
+	{
+		swapped = false;
+		for (int j = 0; j < len - (i+1); ++j)
+		{
+			if (data.at(j) > data.at(j+1))
+			{
+				std::swap(data.at(j), data.at(j+1));
+				swapped = true;
+			}
+		}
+		if (!swapped)
+			break;
+	}
+}
+
+#endif
